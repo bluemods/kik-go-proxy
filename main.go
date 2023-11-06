49f6c9dfc4c84e408a77dbcc8ea1681905cde3c3
Blue Mods <53561555+bluemods@users.noreply.github.com>
2023-11-05 21:46:29 -0500

    Add interface support

--- main.go
+++ main.go
@@ -4,9 +4,8 @@ import (
 	"crypto/tls"
 	"errors"
 	"flag"
-	"fmt"
+	"log"
 	"net"
-	"os"
 	"time"
 )
 
@@ -61,8 +60,7 @@ func main() {
 func openSSLServer(port string, certFile string, keyFile string) {
 	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
 	if err != nil {
-		fmt.Println("Error loading key pair:", err.Error())
-		os.Exit(1)
+		log.Fatal("Error loading key pair:", err.Error())
 	}
 	// Only accepting TLSv1.3 for now, as it's the most secure.
 	// All modern clients should support this.
@@ -70,15 +68,14 @@ func openSSLServer(port string, certFile string, keyFile string) {
 	config := &tls.Config{Certificates: []tls.Certificate{cert}, MinVersion: tls.VersionTLS13}
 	server, err := tls.Listen(SERVER_TYPE, ":"+port, config)
 	if err != nil {
-		fmt.Println("Error opening SSL socket:", err.Error())
-		os.Exit(1)
+		log.Fatal("Error opening SSL socket:", err.Error())
 	}
 	defer server.Close()
-	fmt.Println("Listening using SSL on :" + port)
+	log.Println("Listening using SSL on :" + port)
 	for {
 		connection, err := server.Accept()
 		if err != nil {
-			fmt.Println("Error accepting: ", err.Error())
+			log.Println("Error accepting: ", err.Error())
 		} else {
 			go handleNewConnection(connection)
 		}
@@ -88,15 +85,14 @@ func openSSLServer(port string, certFile string, keyFile string) {
 func openPlainServer(port string) {
 	server, err := net.Listen(SERVER_TYPE, ":"+port)
 	if err != nil {
-		fmt.Println("Error opening unencrypted socket:", err.Error())
-		os.Exit(1)
+		log.Fatal("Error opening unencrypted socket:" + err.Error())
 	}
 	defer server.Close()
-	fmt.Println("Listening unencrypted on :" + port)
+	log.Println("Listening unencrypted on :" + port)
 	for {
 		connection, err := server.Accept()
 		if err != nil {
-			fmt.Println("Error accepting: ", err.Error())
+			log.Println("Error accepting: " + err.Error())
 		} else {
 			go handleNewConnection(connection)
 		}
@@ -107,7 +103,7 @@ func handleNewConnection(clientConn net.Conn) {
 	ipAddress, _, err := net.SplitHostPort(clientConn.RemoteAddr().String())
 	if err != nil {
 		// Shouldn't happen but being safe
-		fmt.Println("Rejecting connection, could not parse remote IP address")
+		log.Println("Rejecting connection, could not parse remote IP address")
 		clientConn.Close()
 		return
 	}
@@ -115,22 +111,22 @@ func handleNewConnection(clientConn net.Conn) {
 	k, err := readKFromClient(clientConn)
 	if err != nil {
 		// TODO: ban hosts
-		fmt.Println("Rejecting from " + ipAddress + ": " + fmt.Sprint(err))
+		log.Println("Rejecting from " + ipAddress + ": " + err.Error())
 		clientConn.Close()
 		return
 	}
 	payload, err := k.makeOutgoingPayload()
 	if err != nil {
 		// TODO: ban hosts
-		fmt.Println("Failed validation " + ipAddress + ": " + fmt.Sprint(err))
+		log.Println("Failed validation " + ipAddress + ": " + err.Error())
 		clientConn.Close()
 		return
 	}
 
-	fmt.Println("Accepting from " + ipAddress + ": " + k.RawStanza)
-	kikConn, err := connectToKik(clientConn, *payload)
+	log.Println("Accepting from " + ipAddress + ": " + k.RawStanza)
+	kikConn, err := connectToKik(clientConn, *payload, "" /* TODO */)
 	if err != nil {
-		fmt.Println("Failed to connect " + ipAddress + " to Kik: " + fmt.Sprint(err))
+		log.Println("Failed to connect " + ipAddress + " to Kik: " + err.Error())
 		clientConn.Close()
 		return
 	}
@@ -159,9 +155,46 @@ func proxy(from net.Conn, to net.Conn) {
 	}
 }
 
-func connectToKik(clientConn net.Conn, sortedKTag string) (*tls.Conn, error) {
-	var config tls.Config = tls.Config{ServerName: KIK_HOST}
-	dialer := net.Dialer{Timeout: KIK_INITIAL_READ_TIMEOUT_SECONDS * time.Second}
+func connectToKik(clientConn net.Conn, sortedKTag string, interfaceName string) (*tls.Conn, error) {
+	config := tls.Config{ServerName: KIK_HOST}
+
+	var dialer net.Dialer
+	if interfaceName != "" {
+		netInterface, err := net.InterfaceByName("eth0")
+		if err != nil {
+			return nil, err
+		}
+		addrs, err := netInterface.Addrs()
+		if err != nil {
+			return nil, err
+		}
+
+		var selectedIP net.IP
+
+		for i := 0; i < len(addrs); i++ {
+			ip := addrs[i].(*net.IPNet).IP
+			if ip.String() == interfaceName {
+				selectedIP = ip
+				break
+			}
+		}
+		if selectedIP == nil {
+			return nil, errors.New("Failed connecting via custom interface; '" + interfaceName + "' not found")
+		}
+
+		tcpAddr := &net.TCPAddr{
+			IP: selectedIP,
+		}
+		dialer = net.Dialer{
+			LocalAddr: tcpAddr,
+			Timeout:   KIK_INITIAL_READ_TIMEOUT_SECONDS * time.Second,
+		}
+	} else {
+		dialer = net.Dialer{
+			Timeout: KIK_INITIAL_READ_TIMEOUT_SECONDS * time.Second,
+		}
+	}
+
 	kikConn, err := tls.DialWithDialer(&dialer, KIK_SERVER_TYPE, KIK_HOST+":"+KIK_PORT, &config)
 	if err != nil {
 		return nil, err
