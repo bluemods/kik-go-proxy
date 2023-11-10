7df76b829527a191a742f5443599294a44827267
Blue Mods <53561555+bluemods@users.noreply.github.com>
2023-11-10 07:54:14 -0500

    fix k parsing bug

--- node.go
+++ node.go
@@ -49,17 +49,57 @@ func ParseNextNode(parser *xpp.XMLPullParser) (*Node, error) {
 	}
 }
 
-// Parse a <k/> string
-// Since initial k tags are not always closed (as they are a stream header)
-// we must manually close them to make the parser work.
-//
-// This limitation may be revisited later.
-func ParseInitialKString(xmpp string) (*Node, error) {
-	fixed := strings.Trim(strings.TrimSuffix(xmpp, "</k>"), " ") + "</k>"
-	if !strings.HasPrefix(fixed, "<k ") {
-		return nil, errors.New("Not a valid k tag\n" + xmpp)
+func ParseInitialK(parser *xpp.XMLPullParser) (*Node, error) {
+	if parser.Event != xpp.StartTag {
+		return nil, errors.New("Expected start tag")
 	}
-	return ParseXmppString(fixed)
+	if parser.Name != "k" {
+		return nil, errors.New("Expected k tag")
+	}
+	var ret = new(Node)
+	ret.Text = ""
+	ret.Name = parser.Name
+	ret.Attributes = map[string]string{}
+	for _, attr := range parser.Attrs {
+		ret.Attributes[attr.Name.Local] = attr.Value
+	}
+	first := true
+
+	for {
+		eventType, err := parser.Next()
+		if err != nil {
+			if first && err == io.EOF {
+				// Stream headers can be unclosed,
+				// this is normal. Return the node.
+				return ret, nil
+			} else {
+				return nil, err
+			}
+		} else if eventType == xpp.StartTag {
+			child, err := ParseNextNode(parser)
+			if err != nil {
+				return nil, err
+			}
+			ret.Children = append(ret.Children, *child)
+		} else if eventType == xpp.Text {
+			ret.Text = parser.Text
+		} else if eventType == xpp.EndTag || eventType == xpp.EndDocument {
+			return ret, nil
+		}
+		first = false
+	}
+}
+
+// Parse a <k/> string.
+// They need special handling as they are not always closed.
+func ParseInitialKString(xmpp string) (*Node, error) {
+	reader := strings.NewReader(xmpp)
+	crReader := func(charset string, input io.Reader) (io.Reader, error) {
+		return input, nil
+	}
+	parser := xpp.NewXMLPullParser(reader, false, crReader)
+	parser.Next()
+	return ParseInitialK(parser)
 }
 
 // Parse an XMPP string
