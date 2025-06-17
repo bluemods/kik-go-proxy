24243a36056107a06cd84e823ca173795d520bd9
Blue <blue@bluesmods.com>
2025-06-17 11:42:27 -0400

    Add new token attributes

--- datatypes/access_token.go
+++ datatypes/access_token.go
@@ -15,14 +15,19 @@ type AccessToken struct {
 	NotBefore time.Time // Cannot be used before this time
 	NotAfter  time.Time // Cannot be used after this time
 
+	Ip           string      // IP used in the last RefreshToken call
 	DeviceId     KikDeviceId // Device ID
-	Jti          string      // JWT UUID
-	HardwareId   string      // Android ID or IDFV
-	RegisteredAt time.Time   // When the account was created
-	Subject      string      // Local part of the JID
-	Type         string      // "a" for access, "r" for refresh
-	Version      string      // Version string, such as `17.6.9-42000`
+	Jti          string      // JWT ID
 	Locale       string      // Locale string, such as "en" or "en_US"
+	HardwareId   string      // Android ID or IDFV
+	Pra          string
+	Psi          string
+	RegisteredAt time.Time // When the account was created
+	Src          string    // Serial refresh counter. This is normally an integer. Only present in refresh token
+	Subject      string    // Local part of the JID
+	Type         string    // "a" for access, "r" for refresh
+	UserType     string    // User type, may be empty
+	Version      string    // Version string, such as `17.6.9-42000`
 }
 
 // Parses the claims in an access token or refresh token JWT.
@@ -42,6 +47,7 @@ func ParseAccessToken(token string) *AccessToken {
 		return nil
 	}
 	var r struct {
+		Ip  string    `json:"_ip"`
 		Aud string    `json:"aud"`
 		Did string    `json:"did"`
 		Exp int64     `json:"exp"`
@@ -51,9 +57,13 @@ func ParseAccessToken(token string) *AccessToken {
 		Jti string    `json:"jti"`
 		Loc string    `json:"loc"`
 		Nbf int64     `json:"nbf"`
+		Pra string    `json:"pra"`
+		Psi string    `json:"psi"`
 		Reg time.Time `json:"reg"`
+		Src string    `json:"src"`
 		Sub string    `json:"sub"`
 		Tpe string    `json:"tpe"`
+		Uty string    `json:"uty"`
 		Ver string    `json:"ver"`
 	}
 	if err := json.Unmarshal(payload, &r); err != nil {
@@ -72,12 +82,17 @@ func ParseAccessToken(token string) *AccessToken {
 		NotBefore:    time.Unix(r.Nbf, 0),
 		NotAfter:     time.Unix(r.Exp, 0),
 		DeviceId:     *deviceId,
+		Ip:           r.Ip,
 		Jti:          r.Jti,
+		Locale:       r.Loc,
 		HardwareId:   r.Hid,
+		Pra:          r.Pra,
+		Psi:          r.Psi,
+		Src:          r.Src,
 		RegisteredAt: r.Reg,
 		Subject:      r.Sub,
 		Type:         r.Tpe,
+		UserType:     r.Uty,
 		Version:      r.Ver,
-		Locale:       r.Loc,
 	}
 }
